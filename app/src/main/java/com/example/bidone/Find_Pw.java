5bdf88480bf91f897daa21b5bae3026400d131c1
sundayjoa <jeonjoo1004@gmail.com>
2023-04-12 08:56:57 +0900

    test

---
+++ app/src/main/java/com/example/bidone/Find_Pw.java
@@ -0,0 +1,13 @@
+package com.example.bidone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Find_Pw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_pw);
+    }
+}
