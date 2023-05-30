7ceeedfa82fe6ff602c32bede5c09d1a02fdafa2
sundayjoa <jeonjoo1004@gmail.com>
2023-05-30 21:28:22 +0900

    requestwriting.xml framlayout과 linerlayout 사용해서 다시 정렬 requestwriting에서 작성 완료 버튼 누르면 팝업 창 뜨도록 설젇 팝업 창에서 날짜 입력 받기 구현

--- app/src/main/java/com/example/bidone/WritingboardActivity.kt
+++ app/src/main/java/com/example/bidone/WritingboardActivity.kt
@@ -1,10 +1,16 @@
 package com.example.bidone
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.view.View
+import android.view.Window
+import android.widget.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class WritingboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +23,35 @@ class WritingboardActivity : AppCompatActivity() {
             finish()
         }
 
+        //완료 버튼 누르면 팝업 창
+        val completeBtn = findViewById<Button>(R.id.completebutton)
+        completeBtn.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.activity_writingdialog)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            //경매 시작 날짜를 정하는 스피너
+            val dateTextView = dialog.findViewById<TextView>(R.id.dateTextView)
+            val datePicker = DatePicker(this)
+            datePicker.minDate = System.currentTimeMillis() - 1000
+            val alertDialog = AlertDialog.Builder(this)
+                .setView(datePicker)
+                .setPositiveButton("OK") { _, _ ->
+                    val calendar = Calendar.getInstance()
+                    calendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+                    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                    dateTextView.text = dateFormat.format(calendar.time)
+                }
+                .create()
+
+            dateTextView.setOnClickListener {
+                alertDialog.show()
+            }
+            
+            //팝업 창 보여주기
+            dialog.show()
+        }
+
         //스피너 관련
         val spinner = findViewById<Spinner>(R.id.spinner2)
         val items = arrayOf("수공예품", "중고물품")
@@ -24,4 +59,5 @@ class WritingboardActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
         }
-    }
+
+     }
---
+++ app/src/main/java/com/example/bidone/WritingdialogActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bidone
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.graphics.Rect
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.TouchDelegate
+import android.view.View
+import android.widget.AdapterView
+import android.widget.DatePicker
+import android.widget.Spinner
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class WritingdialogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_writingdialog)
+
+    }
+}
