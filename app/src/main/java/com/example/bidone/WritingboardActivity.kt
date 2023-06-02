33841590ae57b58c5537a235de204d56b887a330
sundayjoa <jeonjoo1004@gmail.com>
2023-06-02 17:31:46 +0900

    게시글 작성 팝업 창 완료

--- app/src/main/java/com/example/bidone/WritingboardActivity.kt
+++ app/src/main/java/com/example/bidone/WritingboardActivity.kt
@@ -2,13 +2,16 @@ package com.example.bidone
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.app.TimePickerDialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.widget.*
+import org.w3c.dom.Text
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -34,7 +37,7 @@ class WritingboardActivity : AppCompatActivity() {
             val dateTextView = dialog.findViewById<TextView>(R.id.dateTextView)
             val datePicker = DatePicker(this)
             datePicker.minDate = System.currentTimeMillis() - 1000
-            val alertDialog = AlertDialog.Builder(this)
+            val datealertDialog = AlertDialog.Builder(this)
                 .setView(datePicker)
                 .setPositiveButton("OK") { _, _ ->
                     val calendar = Calendar.getInstance()
@@ -45,7 +48,78 @@ class WritingboardActivity : AppCompatActivity() {
                 .create()
 
             dateTextView.setOnClickListener {
-                alertDialog.show()
+                datealertDialog.show()
+            }
+
+            //경매 시작 시간을 정하는 스피너
+            val timeTextView = dialog.findViewById<TextView>(R.id.timeTextView)
+            val timePicker = TimePicker(this)
+            val timealertDialog = AlertDialog.Builder(this)
+                .setView(timePicker)
+                .setPositiveButton("OK") { _, _ ->
+                    val hour = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) timePicker.hour else timePicker.currentHour
+                    val minute = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) timePicker.minute else timePicker.currentMinute
+                    val calendar = Calendar.getInstance()
+                    calendar.set(Calendar.HOUR_OF_DAY, hour)
+                    calendar.set(Calendar.MINUTE, minute)
+                    val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+                    timeTextView.text = timeFormat.format(calendar.time)
+                }
+                .create()
+
+            timeTextView.setOnClickListener {
+                timealertDialog.show()
+            }
+
+            //Text에 아무것도 입력되지 않았다면 오류 메시지 띄우기
+            val datetext = dialog.findViewById<TextView>(R.id.dateTextView)
+            val timetext = dialog.findViewById<TextView>(R.id.timeTextView)
+            val starteditText = dialog.findViewById<EditText>(R.id.startText)
+            val increaseeditText = dialog.findViewById<EditText>(R.id.increaseText)
+            val nextButton = dialog.findViewById<Button>(R.id.completebtn)
+            nextButton.setOnClickListener {
+                val datetextView = datetext.text.toString()
+                val timetextView = timetext.text.toString()
+                val starttext = starteditText.text.toString()
+                val increasetext = increaseeditText.text.toString()
+
+                //날짜 선택 if문
+                if (datetextView.isEmpty()){
+                    //textView가 비어 있는 경우 경고 메시지를 표시.
+                    Toast.makeText(this, "날짜를 반드시 입력해야 합니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    //textView가 비어 있지 않은 경우 다음 단계로 진행
+                }
+
+                //시간 선택 if문
+                if (timetextView.isEmpty()){
+                    //textView가 비어 있는 경우 경고 메시지를 표시
+                    Toast.makeText(this, "시간을 반드시 입력해야 합니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    //textView가 비어 있지 않은 경우 다음 단계로 진행
+                }
+
+                //시작 가격 if문
+                if (starttext.isEmpty()) {
+                    // EditText가 비어 있는 경우 경고 메시지를 표시.
+                    Toast.makeText(this, "시작 가격을 반드시 입력해야 합니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    // EditText가 비어 있지 않은 경우 다음 단계로 진행
+                }
+
+                //증가 가격 if문
+                if(increasetext.isEmpty()){
+                    // EditText가 비어 있는 경우 경고 메시지를 표시.
+                    Toast.makeText(this, "증가 가격을 반드시 입력해야 합니다.", Toast.LENGTH_SHORT).show()
+                } else{
+                    // EditText가 비어 있지 않은 경우 다음 단계로 진행
+                }
+            }
+
+           //취소 버튼 누르면 팝업 창 닫기
+            val cancelbtn = dialog.findViewById<Button>(R.id.cancelbtn)
+            cancelbtn.setOnClickListener() {
+                dialog.dismiss()
             }
             
             //팝업 창 보여주기
--- app/src/main/java/com/example/bidone/WritingdialogActivity.kt
+++ app/src/main/java/com/example/bidone/WritingdialogActivity.kt
@@ -2,15 +2,13 @@ package com.example.bidone
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.TouchDelegate
 import android.view.View
-import android.widget.AdapterView
-import android.widget.DatePicker
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -18,6 +16,6 @@ class WritingdialogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writingdialog)
+        }
 
     }
-}
