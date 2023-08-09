a2f9051869379ec3dc81d58994330ef2f32c17b9
sundayjoa <130355781+sundayjoa@users.noreply.github.com>
2023-08-09 09:56:20 +0900

    php 파일 업로드
    boardinfo.php와 random_number.php 업로드.(나중에 코드 수정할 수도 있음. 본인 mysql 설정에 맞게 바꿔서 사용하기)

---
+++ boardinfo.php
@@ -0,0 +1,33 @@
+<?php
+
+	$con = mysqli_connect("127.0.0.1:3306", "root", "", "bidone");
+	mysqli_query($con, 'SET NAMES utf8');
+	$work_number = $_POST["worknumber"];
+	error_log("worknumber: " . $work_number);
+
+	$title = $_POST["title"];
+	$userID = $_POST["userID"];
+	$thumbnail = $_POST["thumbnail"];
+	$simple_explanation = $_POST["simple_explanation"];
+	$category = $_POST["category"];
+	$detail_explanation = $_POST["detail_explanation"];
+	$detail_image = $_POST["detail_image"];
+	$date = $_POST["datetextView"];
+	$time = $_POST["timetextView"];
+	$start = $_POST["starttext"];
+	$increase = $_POST["increasetext"];
+	$upload_date = $_POST["upload"];
+
+	$thumbnail = strip_tags($_POST["thumbnail"]);
+	$decodedImage = base64_decode($thumbnail);
+
+	$statement = mysqli_prepare($con, "INSERT INTO board VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+
+	mysqli_stmt_bind_param($statement,"sssssssssssss",$work_number,$title,$userID,$thumbnail,$simple_explanation,$category,$detail_explanation,$detail_image,$date,$time,$start,$increase,$upload_date);
+	mysqli_stmt_execute($statement);
+
+	$response = array();
+	$response["success"] = true;
+
+	echo json_encode($response);
+?>
---
+++ random_number.php
@@ -0,0 +1,29 @@
+<?php
+    
+    $host = "127.0.0.1:3306";
+    $user = "root";
+    $password = "";
+    $database = "bidone";
+
+    
+    $conn = new mysqli($host, $user, $password, $database);
+	
+	if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $found = false;
+    while (!$found) {
+        $number = rand(1000000000, 9999999999);
+        $sql = "SELECT * FROM board WHERE work_number=$number";
+	$result = $conn->query($sql);
+        if ($result->num_rows == 0) {
+            // Number is not in use
+            $found = true;
+        }
+    }
+
+    echo $number;
+
+    $conn->close();
+?>
