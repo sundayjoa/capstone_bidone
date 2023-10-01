d11312117b112f56ece5c8c092f4ec34a5fdee4c
sundayjoa <130355781+sundayjoa@users.noreply.github.com>
2023-10-01 14:01:50 +0900

    Add files via upload

---
+++ auctionboard.php
@@ -0,0 +1,29 @@
+<?php
+	$host = "127.0.0.1:3306";
+	$user = "root";
+	$password = "";
+	$database = "bidone";
+
+	$conn = new mysqli($host, $user, $password, $database);
+
+	if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "SELECT title, simple_explanation, upload_date, userID FROM board ORDER BY upload_date DESC";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+   	 $rows = array();
+	
+ 	   while ($row = $result->fetch_assoc()) {
+  	      $rows[] = $row;
+ 	   }
+
+  	  echo json_encode($rows);
+	} else {
+  	  echo json_encode(null);
+	}
+
+	$conn->close();
+?>
