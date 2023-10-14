55de8ae76b7729227b54ed6f9832e1d56b5b3957
sundayjoa <jeonjoo1004@gmail.com>
2023-10-14 20:59:47 +0900

    회원가입 시스템 완료

--- app/src/main/java/com/example/bidone/MembershipActivity.kt
+++ app/src/main/java/com/example/bidone/MembershipActivity.kt
@@ -2,11 +2,14 @@ package com.example.bidone
 
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -29,7 +32,7 @@ class MembershipActivity : AppCompatActivity() {
 
             val id = idText.text.toString()
 
-            // 텍스트가 비어있지 않은지 확인합니다.
+            // 텍스트가 비어있지 않은지 확인
             if (id.isNotEmpty()) {
                 //DB에 데이터 전송하기
                 val request = object : StringRequest(
@@ -40,20 +43,34 @@ class MembershipActivity : AppCompatActivity() {
                         Log.d("Response", response)
                         Log.d("JSON Data", response)
 
-                        val checkData = response.toString()
+                        if (response.contains("exist") || response.startsWith("exist")
+                            || response.endsWith("exist")) {
+                            Toast.makeText(
+                                this,
+                                "이미 존재하는 id 입니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
 
-                        if (checkData == "exist") {
-                            //응답이 exist면 이미 있는 ID 입니다. 라는 메시지 창을 띄웁니다.
-                            Toast.makeText(this, "이미 있는 ID 입니다.", Toast.LENGTH_SHORT).show()
-                        } else if (checkData == "not exist") {
-                            //응답이 not exist면 사용할 수 있는 ID 입니다. 라는 메시지 창을 띄웁니다.
-                            Toast.makeText(this, "사용할 수 있는 ID 입니다.", Toast.LENGTH_SHORT).show()
+                            checkText.setTextColor(Color.parseColor("#969696"))
 
+                        } else if (response.contains("true") || response.startsWith("true")
+                            || response.endsWith("true")) {
+                            Toast.makeText(
+                                this,
+                                "사용할 수 있는 id 입니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            checkText.setTextColor(Color.BLUE)
                         }
-                    },
-                    Response.ErrorListener { error ->
-                        // 요청에 실패하면 오류 메시지를 출력합니다.
-                        Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show()
+                        },
+                    com.android.volley.Response.ErrorListener { error ->
+                        Toast.makeText(
+                            this,
+                            "An error occurred: ${error.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
                     }) {
                     // 요청에 id 값을 전달합니다.
                     override fun getParams(): Map<String, String> {
@@ -64,12 +81,36 @@ class MembershipActivity : AppCompatActivity() {
                 }
                 val queue = Volley.newRequestQueue(this)
                 queue.add(request)
+
             } else {
                 // 텍스트가 비어있다면 입력하라는 메시지를 출력합니다.
                 Toast.makeText(this, "아이디를 입력하세요.", Toast.LENGTH_SHORT).show()
             }
 
         }
+
+        val passwdText = findViewById<EditText>(R.id.passwdText)
+        val passwdcheckText = findViewById<EditText>(R.id.passwdcheckText)
+        val pwcheckText = findViewById<TextView>(R.id.pwcheckText)
+
+        //비밀번호 중복 확인
+        val pwcheckBtn = findViewById<Button>(R.id.pwcheckBtn)
+        pwcheckBtn.setOnClickListener{
+
+            val password = passwdText.text.toString()
+            val confirmPassword = passwdcheckText.text.toString()
+
+            if (password == confirmPassword) {
+
+                Toast.makeText(this, "중복 확인 완료", Toast.LENGTH_SHORT).show()
+                pwcheckText.setTextColor(Color.BLUE)
+
+            } else {
+                Toast.makeText(this, "입력하신 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
 
         // 팝업창 취소 버튼 클릭 이벤트
         val membershipBtn = findViewById<Button>(R.id.membershipBtn)
@@ -83,7 +124,7 @@ class MembershipActivity : AppCompatActivity() {
             val birth = findViewById<TextView>(R.id.birthText)
             val phone = findViewById<TextView>(R.id.phoneText)
             //회원가입 조건 검사하기
-            val checkText = findViewById<TextView>(R.id.checkText)
+            val check = findViewById<TextView>(R.id.checkText)
             val pwcheckText = findViewById<TextView>(R.id.pwcheckText)
 
             val nameText = name.text.toString()
@@ -96,7 +137,7 @@ class MembershipActivity : AppCompatActivity() {
 
                 //전체 확인 후 저장
                 if (nameText.length in 2..20) { // 이름이 2~20글자인 경우
-                    if (checkText.getCurrentTextColor() == Color.BLUE) { // checkText의 색이 파란색인 경우
+                    if (check.getCurrentTextColor() == Color.BLUE) { // checkText의 색이 파란색인 경우
                         if (pwcheckText.getCurrentTextColor() == Color.BLUE) { // pwcheckText의 색이 파란색인 경우
                             if (birthText.length == 6) { // birthText의 길이가 6인 경우
                                 // 회원가입을 진행
